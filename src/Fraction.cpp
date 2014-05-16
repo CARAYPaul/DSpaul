0075a7329aa1c1624e4a085030934c27b4f24287
paul <paul@paul-U36SD>
2014-05-16 15:40:08 +0100

    Debut

---
+++ src/Fraction.cpp
@@ -0,0 +1,18 @@
+/*
+ * Fraction.cpp
+ *
+ *  Created on: 16 mai 2014
+ *      Author: paul
+ */
+
+#include "Fraction.hpp"
+
+Fraction::Fraction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Fraction::~Fraction() {
+	// TODO Auto-generated destructor stub
+}
+
