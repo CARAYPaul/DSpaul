72626945e8035e1ba8d5d113bbffc4bf264bb0d8
paul <paul@paul-U36SD>
2014-05-16 16:36:46 +0100

    ex2_complet

--- test/test.cpp
+++ test/test.cpp
@@ -12,3 +12,17 @@ TEST(stringAfficher, afficher)
 	EXPECT_EQ("3/2",fraction->Afficher());
 }
 
+TEST(stringAfficher, addition)
+{
+	Fraction *fraction = new Fraction(1,2);
+	fraction->addition(1);
+	EXPECT_EQ("3/2",fraction->Afficher());
+}
+
+TEST(stringAfficher, additionfraction)
+{
+	Fraction *fraction = new Fraction(1,2);
+	Fraction *fraction1 = new Fraction(4,2);
+	fraction->addition(*fraction1);
+	EXPECT_EQ("10/4",fraction->Afficher());
+}
